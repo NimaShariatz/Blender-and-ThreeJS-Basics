43a8d7b2b3ad1bc46b4e19285dde28686b15a7c2
NimaShariatz <shariatz@ualberta.ca>
2026-04-06 15:02:40 -0600

    setup work

---
+++ src/components/navbar.tsx
@@ -0,0 +1,33 @@
+import "./navbar.css"
+
+
+interface NavbarProps{
+  daySelect: number;
+  daySelectFunction: (input: number) => void;
+}
+
+
+
+function Navbar({daySelect, daySelectFunction}: NavbarProps) {
+  
+
+  
+  return (
+    <div className="dayPicker_container">
+
+      <button onClick={ ()=> daySelectFunction(1)} style={{ backgroundColor: (daySelect==1) ? "var(--yellow)" : "transparent", opacity: (daySelect!=1) ? "0.35" : "1" }}>
+        Day 1
+      </button>
+      <button onClick={ ()=> daySelectFunction(2)} style={{ backgroundColor: (daySelect==2) ? "var(--yellow)" : "transparent", opacity: (daySelect!=2) ? "0.35" : "1" }}>
+        Day 2
+      </button>
+      <button onClick={ ()=> daySelectFunction(3)} style={{ backgroundColor: (daySelect==3) ? "var(--yellow)" : "transparent", opacity: (daySelect!=3) ? "0.35" : "1" }}>
+        Day 3
+      </button>
+
+
+    </div>
+  )
+}
+
+export default Navbar;
--- src/pages/tutorial_page.tsx
+++ src/pages/tutorial_page.tsx
@@ -1,8 +1,63 @@
+import "./tutorial_page.css"
+import { useState } from "react";
+
+import Navbar from "../components/navbar";
+
 function TutorialPage() {
+  
+  const [daySelect, setDaySelect] = useState(1);
+
+
+
+  const changeDay = (input: number) => {
+    setDaySelect(input)
+  }
+
+  
   return (
-    <div>
-      <h1 style={{paddingBlock:"1000px"}}>Tutorial Page 1</h1>
+    <>
+
+
+    <Navbar daySelect={daySelect} daySelectFunction={changeDay}/>
+
+
+
+
+    <div className="contentSection_container">
+
+      <div className="titleSection_container">
+        <h1>Day 1</h1>
+      </div>
+
+      <div className="content_container">
+        <h2>Goal</h2>
+        <small>For Day 1</small>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil ipsa, mollitia corporis dolorum sequi molestias accusamus rem vitae cumque nostrum eius qui. Ullam in nihil hic quos distinctio. Ullam, est.</p>
+      </div>
+
+      <div className="titleSection_container">
+        <h1>Key Bindings</h1>
+      </div>
+
+      <div className="content_container">
+
+      </div>
+
+
+      <div className="content_container">
+
+      </div>
+
+
+      {/* Movement keybindings */}
+      
+      {/* mesh-related Keybindings */}
+
+      {/* menu-related content */}
     </div>
+
+
+    </>
   )
 }
 
